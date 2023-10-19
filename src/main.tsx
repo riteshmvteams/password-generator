2e564cb7657dbfe6e710671f6558f61bc1115db9
riteshmvt <ritesh.mvteams@gmail.com>
2023-10-19 12:48:33 +0530

    ui changes

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.scss";
+// context
+import PasswordProvider from "./context/PasswordContext.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <PasswordProvider>
+      <App />
+    </PasswordProvider>
   </React.StrictMode>
 );
